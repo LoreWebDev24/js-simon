f99e1a67f9645b93a36d273df3157e9fc3b2099b
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-17 14:40:31 +0200

    imported main functions  and started styling excs

---
+++ js/master.js
@@ -0,0 +1,34 @@
+// MY FUNCTIONS : 
+
+
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min + 1) + min) 
+}
+
+function getRandomArrayOfInts (minRange, maxRange, number) { 
+    const intArray = []
+
+    while (intArray.length < number) {
+        const n = getRandomIntInclusive(minRange,maxRange)
+
+        if (!intArray.includes(n)) {
+            intArray.push(n)
+        }
+    }
+
+    return intArray
+}
+
+// DOM ELEMENYS : 
+let numbersDOMElement = document.getElementById('numbers__p')
+console.log(numbersDOMElement)
+
+let intArray = getRandomArrayOfInts (1, 100, 5)
+console.log(intArray)
+
+
+// APP JS: 
+
+numbersDOMElement.innerHTML += intArray
